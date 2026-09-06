5b84d44c7e444873de4418edb7f0625cf4e5324c
eymeric <hatchchien@protonmail.com>
2026-09-06 21:37:29 +0700

    kvmd: 4.212 -> 4.213

--- packages/kvmd.nix
+++ packages/kvmd.nix
@@ -32,13 +32,13 @@
 }:
 python314.pkgs.buildPythonApplication rec {
   pname = "kvmd";
-  version = "4.212";
+  version = "4.213";
 
   src = fetchFromGitHub {
     owner = "pikvm";
     repo = "kvmd";
     tag = "v${version}";
-    sha256 = "sha256-3tRd9/h08c5ofvFkBXLHBkFZr8s0yeWrY3qNh3Pzxi4=";
+    sha256 = "sha256-uG6evPjc1pGovLk3ZQjCvpvhjKnmLij2bN7AcqA7t/Q=";
   };
   pyproject = true;
   build-system = with python314.pkgs; [ setuptools ];
